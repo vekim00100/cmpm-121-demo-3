64888312836b6ad5cd9582b2f735370f31cb2f44
vekim00100 <vekim@ucsc.edu>
2024-12-04 12:42:54 -0800

    L16 Dialog: Add button for alert functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 // todo
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const button = document.createElement("button");
+button.innerHTML = "click me!";
+
+button.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+
+app.append(button);
